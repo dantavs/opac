4e7e0ec0b0acc17537643f6c8e38b493bf58ff81
Daniel Tavares <dantavs.gmail.com>
2023-10-15 10:06:24 +0000

    First deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { createServer } from 'node:http'
+
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, GET, DELETE",
+    "Access-Control-Max-Age": 2592000
+}
+
+const app = createServer()
+    .listen(process.env.PORT ? Number(process.console.env.PORT) : 3333
+        , () => console.log('App is running!'))
+
 console.log("Hello One Piece Awesome Cardgame!!")
