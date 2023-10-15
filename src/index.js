d8d89dead00cd84408eb92e95afe158011255a90
Daniel Tavares <dantavs.gmail.com>
2023-10-15 13:10:21 +0000

    frontend connection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { createServer } from 'node:http'
+import { stringify } from 'node:querystring'
 
 const headers = {
     "Access-Control-Allow-Origin": "*",
@@ -6,7 +7,12 @@ const headers = {
     "Access-Control-Max-Age": 2592000
 }
 
-const app = createServer()
+async function handler(request, response){
+    response.writeHead(200, headers)
+    response.end(JSON.stringify({message: 'Game Server if on!'}))
+}
+
+const app = createServer(handler)
     .listen(process.env.PORT ? Number(process.env.PORT) : 3333
         , () => console.log('App is running!'))
 
