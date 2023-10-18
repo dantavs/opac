8209894385b869d88b5bc9e23601ccd17f9f2d88
Daniel Tavares <dantavs.gmail.com>
2023-10-18 13:29:51 +0000

    img change

--- src/game-data/one-piece-deck.js
+++ src/game-data/one-piece-deck.js
@@ -1,10 +1,16 @@
 import { Card } from '../entities/card.js'
 
-const card1 = new Card("Luffy", 5000, "https://freepngimg.com/download/one_piece/23457-1-one-piece-luffy-transparent-background.png")
-const card2 = new Card("Zoro", 4000, "https://icons.veryicon.com/png/Movie%20%26%20TV/One%20Piece%20Manga/Roronoa%20Zoro.png")
-const card3 = new Card("Sanji", 3000, "https://i.pinimg.com/originals/ee/cc/8d/eecc8d361519737d8d717d034f841521.png")
-const card4 = new Card("Nami", 2000, "https://onepiecelendas.files.wordpress.com/2012/08/one_piece_cutout_by_dipset153.png")
-const card5 = new Card("Usopp", 1000, "https://i.pinimg.com/originals/ce/99/94/ce999496043af1f0f09489ac2272d957.png")
+const card1 = new Card("Luffy", 5000, "https://cf.shopee.ph/file/78a0c7b55a6c057bf2f2aa211c73e55b")
+const card2 = new Card("Zoro", 4000, "https://i.pinimg.com/originals/90/a0/cd/90a0cd2c455fd06cd36c8a994bb213d2.png")
+const card3 = new Card("Sanji", 3000, "https://www.vippng.com/png/detail/128-1281868_sanji-pirate-logo-2-by-brian-one-piece.png")
+const card4 = new Card("Nami", 2000, "https://www.pngitem.com/pimgs/m/27-273129_one-piece-logo-png-one-piece-nami-flag.png")
+const card5 = new Card("Usopp", 1000, "https://a0.anyrgb.com/pngimg/1244/1282/timeskip-trafalgar-d-water-law-sanji-jolly-roger-vinsmoke-sanji-usopp-monkey-d-luffy-straw-hat-nami-roronoa-zoro-thumbnail.png")
+
+//const card1 = new Card("Luffy", 5000, "https://freepngimg.com/download/one_piece/23457-1-one-piece-luffy-transparent-background.png")
+//const card2 = new Card("Zoro", 4000, "https://icons.veryicon.com/png/Movie%20%26%20TV/One%20Piece%20Manga/Roronoa%20Zoro.png")
+//const card3 = new Card("Sanji", 3000, "https://i.pinimg.com/originals/ee/cc/8d/eecc8d361519737d8d717d034f841521.png")
+//const card4 = new Card("Nami", 2000, "https://onepiecelendas.files.wordpress.com/2012/08/one_piece_cutout_by_dipset153.png")
+//const card5 = new Card("Usopp", 1000, "https://i.pinimg.com/originals/ce/99/94/ce999496043af1f0f09489ac2272d957.png")
 
 export const onePieceGameDeck = [
     card1, card2, card3, card4, card5
