3882ba23e17ee5edfecff4e8897d454138a48328
Daniel Tavares <dantavs.gmail.com>
2023-10-23 16:08:50 +0000

    next round preparation

--- src/controllers/one-piece-game-controller.js
+++ src/controllers/one-piece-game-controller.js
@@ -1,4 +1,4 @@
-import { headers } from '../index.js'
+import { headers, prisma } from '../index.js'
 import { OPGNextRound } from '../services/opg-next-round.js'
 import { OPGStart } from '../services/opg-start.js'
 import { JKGStart } from '../services/jkg-start.js'
@@ -15,6 +15,23 @@ export async function OPGStartController(request, response){
 
 export async function OPGNextRoundController(request, response){    
     const { deck, playerA, playerB, status } = JSON.parse(await once(request, 'data'))
+
+    const gameId = ""
+    const playedCardId = ""
+
+    //get game
+    const gameDb = prisma.games.findUnique({
+        where: {
+            id: gameId
+        }
+    })
+    //get playerd ID and played card ID
+    //select radomly the pc played card
+    //check round winner
+    //update loser HP
+    //check if game ended
+    //update decks
+    //respond
 
     const game = new Game(onePieceGameDeck, playerA, playerB, status)
     game.deck.shuffle()
